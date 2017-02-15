ccde2b9d1c64a2d2a6fc2dc0cbb082ce50ed2c91
НургалиевД <dds861@gmail.com>
2017-02-15 17:55:15 +0300

    123

---
+++ vev/src/main/resources/Controller/LoginServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+/**
+ * Created by НургалиевД on 15.02.2017.
+ */
+public class LoginServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+
+
+}

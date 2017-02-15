e329a093d7b10c8b757aaea20cee848d6863f27f
НургалиевД <dds861@gmail.com>
2017-02-15 18:26:18 +0300

    123

--- vev3/src/Controller/Student.java
+++ vev3/src/Controller/Student.java
@@ -4,4 +4,23 @@ package Controller;
  * Created by НургалиевД on 15.02.2017.
  */
 public class Student {
+
+    private String id;
+    private String group;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
 }

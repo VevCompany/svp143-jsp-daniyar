fd698b6d1e04c6f00d4a172ca1c44960d15c8ccc
dds861 <dds861@Gmail.com>
2017-02-15 18:36:45 +0600

    1

--- vev/src/main/resources/Student.kt
+++
@@ -1,5 +0,0 @@
-/**
- * Created by D.Nurgaliyev on 15.02.2017.
- */
-class Student {
-}
---
+++ vev/src/main/resources/StudentServlet.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by D.Nurgaliyev on 15.02.2017.
+ */
+public class StudentServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
